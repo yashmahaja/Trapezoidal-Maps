2fea0b61b25d4aa1096754711ead6d5a46c44daf
Yash Mahajan <58283608+yashmahaja@users.noreply.github.com>
2024-11-05 17:27:41 -0500

    Bounding Box Created

--- PA3.py
+++ PA3.py
@@ -20,6 +20,12 @@ class Trapezoid:
         self.neighbors = {}  # Store neighboring trapezoids if needed
 
 
+    def __repr__(self):
+        return (f"Trapezoid(top={self.top}, bottom={self.bottom}, "
+                f"left={self.left}, right={self.right}, "
+                f"neighbors={list(self.neighbors.keys())})")
+
+
 class Solution:
     def __init__(self):
         # self.dag = DAG()  # DAG to represent the trapezoidal map
@@ -34,10 +40,25 @@ class Solution:
                 segment = (Node(x1,y1), Node(x2,y2))
                 segments.append(segment)
         print(segments)
+        self.bounding_box(segments)
         return segments
 
-    def bounding_box(self):
-        pass
+    def bounding_box(self,segments):
+        min_x = min(segments[0][0].x, segments[0][1].x)
+        max_x = max(segments[0][0].x, segments[0][1].x)
+        min_y = min(segments[0][0].y, segments[0][1].y)
+        max_y = max(segments[0][0].y, segments[0][1].y)
+
+        for segment in segments:
+            for node in segment:
+                min_x = min(min_x, node.x)
+                max_x = max(max_x, node.x)
+                min_y = min(min_y, node.y)
+                max_y = max(max_y, node.y)
+        bounding_trapezoid = Trapezoid(top=max_y + 5, bottom=min_y - 5, left=min_x - 5 , right=max_x + 5)
+        print("Bounding box:", bounding_trapezoid)
+        return bounding_trapezoid
+
     def build_trapezoidal_map(self, segments):
         # Implements the incremental construction algorithm
         pass
@@ -50,13 +71,8 @@ class Solution:
 def main():
     solution = Solution()
 
-    segments = solution.get_input("InputFiles/InputFiles/ya2390.txt")  # Replace with the actual input file name
+    segments = solution.get_input("InputFiles/ya2390.txt")  # Replace with the actual input file name
     solution.build_trapezoidal_map(segments)
-
-    # Example of querying a point
-    x, y = 3, 4  # Replace with actual query values
-    region = solution.query_point(x, y)
-    print("Region:", region)
 
 
 if __name__ == '__main__':
